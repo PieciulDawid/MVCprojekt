ffa6fbdbc0917bdb7cf4986a0cc5e7e89748cf67
Dawid Pieciul <pieciul.dawid@gamil.com>
2022-02-01 17:53:58 +0100

    Koszyk

--- MVCprojekt/MVCprojekt/Controllers/HomeController.cs
+++ MVCprojekt/MVCprojekt/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity.Owin;
@@ -8,6 +9,7 @@ namespace MVCprojekt.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
             //Lista produktów
@@ -29,6 +31,19 @@ namespace MVCprojekt.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductModel productModel = db.ProductModels.Find(id);
+            if (productModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productModel);
         }
     }
 }
