3aba3297c446245fa53fcac664e61ecd9bc3993b
Dawid Pieciul <pieciul.dawid@gamil.com>
2022-02-01 01:17:16 +0100

    Wysyłanie maili przez zalogowanego użytkownika

---
+++ MVCprojekt/MVCprojekt/Controllers/EmailController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MVCprojekt.Controllers
+{
+    public class EmailController : Controller
+    {
+        // GET: Email
+        public ActionResult Contact()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Contact(string body)
+        {
+            //TODO tu wstawić email użytkownika naj jaki można odesłać odpoweidź
+            string subject = "Email użytkownika";
+            string useremail = "aspnetmvcprojekt@gmail.com";
+            WebMail.Send(useremail, subject, body, null, null, null, true, null, null, null, null, null, null);
+            ViewBag.msg = "Wysłano email";
+            return View();
+        }
+    }
+}
